ea51c02a39dd24f30cfa6852eb3ff1604262104b
Samara Silva <samarasilva.dev@gmail.com>
2022-06-21 21:13:48 -0300

    Segundo exercício básico de select

--- Basic Select/revising_the_select_query_II.sql
+++ Basic Select/revising_the_select_query_II.sql
@@ -1,8 +1,8 @@
 /* 
     Query the NAME field for all American cities in the CITY 
-    table with populations larger than 120000. The CountryCode 
-    for America is USA.
-*/
+    table with populations larger than 120000. 
+    The CountryCode for America is USA.
+ */
 
 SELECT name
 FROM city
